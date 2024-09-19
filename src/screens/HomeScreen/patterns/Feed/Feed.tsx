8e821ba2077b279a1ef329d2710710520d87e43b
Gustavo Barreto <gbarreto.bastos@gmail.com>
2024-09-19 13:16:05 -0300

    inserindo imagem

--- src/screens/HomeScreen/patterns/Feed/Feed.tsx
+++ src/screens/HomeScreen/patterns/Feed/Feed.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Box from "@src/components/Box/Box";
 import Text from "@src/components/Text/Text";
 import Icon from "@src/components/Icon/Icon";
+import Image from "@src/components/Image/Image";
 
 interface FeedProps {
     children: React.ReactNode;
@@ -25,6 +26,15 @@ Feed.Header = () => {
             color: 'white',
         }}
         >
+            <Image 
+            styleSheet={{
+                width: '128px',
+                heigth: '128px',
+                borderRadius: '100%',
+            }}
+            src="https://github.com/GustavoBarretoLima.png"
+            alt="Imagem de perfil Gustavo Barreto"
+            />
             <Icon name = "youtube" />
             <Icon name = "twitch" />
             <Icon name = "instagram" />
